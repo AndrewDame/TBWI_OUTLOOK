569eb2c238a747c4311469a382a5aeb11594bc18
Andrew Dame <andrewdame@ip-97-93.wireless.oberlin.edu>
2018-11-08 13:02:57 -0500

    Updated as of 11-08-18

---
+++ TBWI-SALES/TBWI-SALES.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grid1 = new System.Windows.Forms.DataGridView();
+            this.Column10 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid1
+            // 
+            this.grid1.BackgroundColor = System.Drawing.SystemColors.MenuHighlight;
+            this.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column10,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.grid1.Location = new System.Drawing.Point(12, 131);
+            this.grid1.Name = "grid1";
+            this.grid1.Size = new System.Drawing.Size(1561, 696);
+            this.grid1.TabIndex = 0;
+            this.grid1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grid1_CellContentClick);
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Sel";
+            this.Column10.Name = "Column10";
+            this.Column10.Width = 20;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Company";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "City";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Type";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Contact";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 150;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Phone";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Email1";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 200;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Email2";
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 200;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Website";
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 150;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Notes";
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 275;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 17);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 108);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(163, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(680, 51);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Send Email";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(896, 30);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 7;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Data";
+            this.label1.UseMnemonic = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(893, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Message Contents";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1565, 839);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.grid1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grid1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TBWI-SALES/TBWI-SALES.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Diagnostics;
+using Outlook = Microsoft.Office.Interop.Outlook;   
+
+namespace WindowsFormsApplication1
+{
+    
+    
+    
+    public partial class Form1 : Form
+    {
+
+
+        string filename = null;   //initalize filename and message 
+        string message = "intro.txt";
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+         DirectoryInfo dinfo = new DirectoryInfo(@"c:\\TrueBearingClientList");  //initalize two listboxes subject to written directory
+         FileInfo[] Files = dinfo.GetFiles("*.txt");
+         foreach (FileInfo file in Files)
+    {
+        listBox1.Items.Add(file.Name);
+    }
+
+         DirectoryInfo dinfo_1 = new DirectoryInfo(@"c:\\TBWI\\Prospects");
+         FileInfo[] Files_1 = dinfo_1.GetFiles("*.txt");
+         foreach (FileInfo file in Files_1)
+         {
+             listBox2.Items.Add(file.Name);
+         }
+        
+        }
+
+
+
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (filename == null)     
+            {
+
+            }
+            
+            else{
+                grid1.Rows.Clear();
+           
+
+
+            string directory = string.Format("c:\\TrueBearingClientList\\{0}", filename);
+            StreamReader salesdata = new StreamReader(directory);
+            string linex;
+            int r = 0;
+            while ((linex = salesdata.ReadLine()) != null)
+            {
+                grid1.Rows.Add();
+                for (int i = 0; i < 9; i++)
+                {
+                    string[] fieldsx = linex.Split(',');
+                    
+                    grid1.Rows[r].Cells[i+1].Value = Convert.ToString(fieldsx[i]); 
+
+                }
+                r++;
+
+            }
+
+            salesdata.Close();
+
+}
+
+            string edit_filename = filename.Replace(".txt", "");                 //create backup of data loaded
+            string directory_1 = string.Format("c:\\TrueBearingClientList\\{0}_backup.txt", edit_filename);
+            if (!(File.Exists(directory_1)))
+            {
+
+                FileStream fs = new FileStream(directory_1, FileMode.CreateNew, FileAccess.Write);
+                StreamWriter tbwi = new StreamWriter(fs);
+
+                int row = grid1.RowCount - 1;
+                for (int i = 0; i < row; i++)
+                {
+                    int j = 1;
+                    for (j = 1; j < 10; j++)
+                    {
+                        tbwi.Write(grid1.Rows[i].Cells[j].Value);
+                        if (j < 9)
+                        {
+                            tbwi.Write(",");
+                        }
+                        if (j == 9)
+                        {
+                            tbwi.WriteLine();
+                        }
+
+
+
+                    }
+                    j = 1;
+                }
+                tbwi.Close();
+
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) //updates changes to current file
+        {
+            string directory = string.Format("c:\\TrueBearingClientList\\{0}", filename);
+            File.Delete(directory);
+            FileStream fs = new FileStream(directory, FileMode.CreateNew, FileAccess.Write);
+            StreamWriter tbwi = new StreamWriter(fs);
+
+            int r = grid1.RowCount - 1;
+            for (int i = 0; i < r; i++)
+            {
+                int j = 1;
+                for (j = 1; j < 10; j++)
+                {
+                    tbwi.Write(grid1.Rows[i].Cells[j].Value);
+                    if (j < 9)
+                    {
+                        tbwi.Write(",");
+                    }
+                    if (j == 9)
+                    {
+                        tbwi.WriteLine();
+                    }
+                    
+
+
+                }
+                j = 1;
+            }
+            tbwi.Close();
+
+        }
+
+  
+
+        private void grid1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)           //send message
+        {
+            
+            string text = string.Format("c:\\TBWI\\Prospects\\{0}", message);
+            string msg = File.ReadAllText(text);
+            Outlook.Application app = new Outlook.Application();
+           
+           
+
+            for (int s = 0; s < grid1.RowCount; s++)
+            {
+                
+                
+                int c = Convert.ToInt32(grid1.Rows[s].Cells[0].Value); 
+                
+                if (c == 1)
+                {
+                    string company = Convert.ToString(grid1.Rows[s].Cells[1].Value);
+                    string contact = Convert.ToString(grid1.Rows[s].Cells[4].Value);
+                    string address = company + " - " + contact;
+                  
+                    //listBox2.Items.Add(Convert.ToString(grid1.Rows[s].Cells[1].Value));
+                    Outlook.MailItem mailitem = app.CreateItem(Outlook.OlItemType.olMailItem);
+                    mailitem.Subject = "C/P Performance Reports, Route Advisories & Performance Monitoring";
+                    mailitem.Body = address + msg;
+                   
+                    mailitem.To = Convert.ToString(grid1.Rows[s].Cells[6].Value);
+                    mailitem.Send();
+
+
+                }
+
+
+            }
+
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            
+            string filename_1 = listBox1.GetItemText(listBox1.SelectedItem);
+            filename = filename_1.Replace(" ", "");
+           
+        }
+
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+            string message_1 = listBox2.GetItemText(listBox2.SelectedItem);
+            message = message_1.Replace(" ", "");
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+       
+
+       
+    
+    
+    }
+}
